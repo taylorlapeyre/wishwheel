b4f2e7cd1202e2d10d7836e3cc2e14ee04fff465
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-11-07 13:49:18 -0600

    more docs

--- src/wishwheel/images.clj
+++ src/wishwheel/images.clj
@@ -1,14 +1,19 @@
 (ns wishwheel.images
+  "Logic for saving images to the filesystem."
   (:require [clojure.java.io :as io]
             [clojure.string :as string])
   (:import java.io.File))
 
 
 (defn get-random-id [length]
+  "Generate a random alphanumeric identifier of with a given length."
   (let [alphanumeric "abcdefghijklmnopqrstuvwxyz1234567890"]
     (apply str (repeatedly length #(rand-nth alphanumeric)))))
 
 (defn random-file-name
+  "Given an existing filename, returns a randomly generated file name
+  using the same file extension. Files are located in:
+  resources/public/images/"
   [filename]
   (let [file-extension (last (string/split filename #"\."))
         directory "resources/public/images/"]
